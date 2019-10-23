90f4ee39e1e7f1392444191ba2d3f7ffbd860f3b
songtm <565038419@qq.com>
2019-10-23 15:06:53 +0800

    show compoents in gameobject treeView

--- AssetStudioGUI/Components/GameObjectTreeNode.cs
+++ AssetStudioGUI/Components/GameObjectTreeNode.cs
@@ -11,6 +11,35 @@ namespace AssetStudioGUI
         {
             this.gameObject = gameObject;
             Text = gameObject.m_Name;
+            System.Text.StringBuilder coms = new System.Text.StringBuilder();
+            coms.Append("  [");
+            foreach (var pptr in gameObject.m_Components)
+            {
+                if (pptr.TryGet(out var com))
+                {
+                    if (com is MonoBehaviour monoBehaviour)
+                    {
+
+                        if (monoBehaviour.m_Name == "" && monoBehaviour.m_Script.TryGet(out var m_Script))
+                        {
+                            coms.Append(m_Script.m_ClassName);
+                        }
+                        else
+                        {
+                            coms.Append(monoBehaviour.m_Name);
+                        }
+                    }else
+                    {
+                        var parts = com.ToString().Split('.');
+                        coms.Append(parts[parts.Length - 1]);
+
+                    }
+                    coms.Append(" ");
+                }
+            }
+            if (coms.Length > 1) coms.Remove(coms.Length - 1, 1);
+            coms.Append("]");
+            Text += coms;
         }
     }
 }
